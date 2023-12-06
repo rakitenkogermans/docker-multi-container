7c866501af5ca7776651465ec4aa01162ce37b2f
rakitenkogermans <rakitenko.germans@gmail.com>
2023-12-06 18:55:05 +0200

    change app text

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Fibonacci calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
